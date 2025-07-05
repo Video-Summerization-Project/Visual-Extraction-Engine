7efdb44ac1ffa51406274d8fd66c2e524e049df2
Amro <amrokahla@yahoo.com>
2025-07-05 12:42:58 +0300

    update keyframe selection

--- main.py
+++ main.py
@@ -6,16 +6,47 @@ from KeyFrameSelection.Similarties import hash_filter, clip_filter
 
 keyframe_dir = 'outputs/keyframes'
 csv_path = 'outputs/keyframes.csv'
-video_path = 'https://drive.google.com/file/d/1B17NM3HS5tlN_gPzZjDlCBa2tKMTMwTN/view?usp=drive_link' # Change this to your video file path
+video_path = 'RawVideos\Filters - Mohammad Ayed (720p, h264).mp4' # Change this to your video file path
 
 def main():
+    # Process the video and extract keyframes
+    records, fps = process_video(video_path, interval_sec=3)
 
-    # Process the video to extract keyframes and their records
-    records, fps = process_video(video_path, interval_sec=3)    # Process the video and extract keyframes
-    filtered_records =hash_filter(records, hash_threshold=5, ssim_threshold=0.95, ssim_compare_window=5)    # Filter out similar keyframes based on hash and SSIM
-    filtered_records = clip_filter(filtered_records, similarity_threshold=0.90, compare_window=5)   # Filter out similar keyframes based on clip similarity
-    save_records(filtered_records, keyframe_dir, csv_path, fps)   # Save the filtered keyframes and their records to the specified directory and CSV file
+    # Initial thresholds
+    hash_threshold = 5
+    ssim_threshold = 0.95
+    clip_threshold = 0.90
 
+    min_frames = 8
+    max_iterations = 10
+    iteration = 0
+
+    filtered_records = records
+    while len(filtered_records) > min_frames and iteration < max_iterations:
+       # print(f"num of iteration {iteration+1}")
+        filtered_records = hash_filter(
+            filtered_records,
+            hash_threshold=hash_threshold,
+            ssim_threshold=ssim_threshold,
+            ssim_compare_window=5
+        )
+
+        filtered_records = clip_filter(
+            filtered_records,
+            similarity_threshold=clip_threshold,
+            compare_window=5
+        )
+
+        # Tighten thresholds
+        hash_threshold = max(1, hash_threshold - 1)
+        ssim_threshold = max(0.5, ssim_threshold - 0.05)
+        clip_threshold = min(0.99, clip_threshold + 0.03)
+
+        iteration += 1
+        #print(f"Num of frames: {len(filtered_records)}")
+
+    # Save the filtered keyframes and their records
+    save_records(filtered_records, keyframe_dir, csv_path, fps)
 
 if __name__ == "__main__":
     main()
